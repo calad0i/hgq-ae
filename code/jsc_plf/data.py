a97a44dfdb013850db22e5d33c31dc7ce7f86729
Chang Sun <chsun@cern.ch>
2025-12-22 19:42:27 -0800

    dataloader fix for jsc plf

--- code/jsc_plf/data.py
+++ code/jsc_plf/data.py
@@ -8,29 +8,26 @@ def get_data(data_path: Path | str, n_constituents: int, ptetaphi: bool, val_siz
     data_path = Path(data_path)
 
     with h5.File(data_path / '150c-train.h5') as f:
-        X_train_val = np.array(f['feature'][:, :n_constituents]).astype(np.float16)  # type: ignore
+        X_train_val = np.array(f['feature'])
         y_train_val = np.array(f['label'])
     with h5.File(data_path / '150c-test.h5') as f:
-        X_test = np.array(f['feature'][:, :n_constituents]).astype(np.float16)  # type: ignore
+        X_test = np.array(f['feature'])
         y_test = np.array(f['label'])
 
-    X_train_val = X_train_val.astype(np.float32)
+    X_train_val = X_train_val[:, :n_constituents]
     y_train_val = y_train_val.astype(np.int32)
-    X_test = X_test.astype(np.float32)
+    X_test = X_test[:, :n_constituents]
     y_test = y_test.astype(np.int32)
+
+    scale = np.std(X_train_val.astype(np.float32), axis=(0, 1), keepdims=True)
+    shift = np.mean(X_train_val.astype(np.float32), axis=(0, 1), keepdims=True)
 
     if ptetaphi:
         X_train_val = X_train_val[..., [5, 8, 11]]
         X_test = X_test[..., [5, 8, 11]]
-        X_train_val *= X_train_val[..., :1] >= 2
-        X_test *= X_test[..., :1] >= 2
 
-    scale = np.std(X_train_val, axis=(0, 1), keepdims=True)
-    shift = np.mean(X_train_val, axis=(0, 1), keepdims=True)
-    X_train_val = (X_train_val - shift) / scale  # type: ignore
-    X_test = (X_test - shift) / scale  # type: ignore
-
-    X_train_val, X_test = X_train_val.astype(np.float16), X_test.astype(np.float16)
+    X_train_val = (X_train_val - shift) / scale
+    X_test = (X_test - shift) / scale
 
     order = np.arange(len(X_train_val))
     np.random.shuffle(order)
